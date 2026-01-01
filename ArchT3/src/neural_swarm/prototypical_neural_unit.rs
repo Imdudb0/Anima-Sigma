c9f373d2f9f5e25772940bc51707cb3eed8de502
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2026-01-01 09:42:54 -0500

    Update prototypical_neural_unit.rs

--- ArchT3/src/neural_swarm/prototypical_neural_unit.rs
+++ ArchT3/src/neural_swarm/prototypical_neural_unit.rs
@@ -1,9 +1,11 @@
 use crate::perception::universal_vector::UniversalVector;
 
 pub struct PrototypicalNeuralUnit {
+    pub id: Uuid,
     pub prototype_weight: UniversalVector,
     pub activation_threshold: f64,
     pub current_activation_energy: f64,
+    pub link: Vec<Uuid>,
 }
 
 impl PrototypicalNeuralUnit {
