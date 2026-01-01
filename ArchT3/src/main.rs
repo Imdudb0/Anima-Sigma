fe22fe0b854999cf06fb0773f01319348fb857d4
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2025-12-31 20:26:11 -0500

    Update main.rs

--- ArchT3/src/main.rs
+++ ArchT3/src/main.rs
@@ -3,7 +3,9 @@ BalanceStickAnimation,
 };
 
 fn main() {
+    let mut simulation = BalanceStickAnimation::new(30.0);
     loop {
-            
+       let dx = simulation.run_for_signature();
+       println!("dx: {:?}", dx);
     }
 }
