ff2ff9a84cfde891a2a3f9cfe621f0748bdb38a6
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2026-01-01 10:16:56 -0500

    Update dictionary.rs

--- ArchT3/src/logic/dictionary.rs
+++ ArchT3/src/logic/dictionary.rs
@@ -1,14 +1,15 @@
+use crate::perception::universal_vector::UniversalVector;
+
 use std::sync::{LazyLock, Mutex};
 use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Dictionary {
-    pub 
+    pub concepts: HashMap<String, Vec<UniversalVector>>
 }
 
 pub static CONCEPTS: LazyLock<Mutex<Dictionary>> = LazyLock::new(|| {
-    Mutex::new(SwarmIndex {
-        tribes: Vec::new(),
-        orphans: Vec::new(),
+    Mutex::new(Dictionary {
+        concepts: HashMap::new(),
     })
 });
