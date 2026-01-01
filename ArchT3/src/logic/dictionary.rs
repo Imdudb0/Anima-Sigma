56c232dfc57c54020c36f5f9036856980a4a93c1
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2026-01-01 10:06:44 -0500

    Update dictionary.rs

--- ArchT3/src/logic/dictionary.rs
+++ ArchT3/src/logic/dictionary.rs
@@ -1,1 +1,14 @@
-.
+use std::sync::{LazyLock, Mutex};
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Dictionary {
+    pub 
+}
+
+pub static CONCEPTS: LazyLock<Mutex<Dictionary>> = LazyLock::new(|| {
+    Mutex::new(SwarmIndex {
+        tribes: Vec::new(),
+        orphans: Vec::new(),
+    })
+});
