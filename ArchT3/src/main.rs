cd69da0798ca8d611ec137aa7b0c050bd289bb5b
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2025-12-31 20:44:38 -0500

    Update main.rs

--- ArchT3/src/main.rs
+++ ArchT3/src/main.rs
@@ -3,9 +3,18 @@ BalanceStickAnimation,
 };
 
 fn main() {
-    let mut simulation = BalanceStickAnimation::new(30.0);
+    let mut simulator = BalanceStickAnimation::new(30.0);
+    let start_time = Instant::now();
+   
     loop {
-       let dx = simulation.run_for_signature();
-       println!("dx: {:?}", dx);
+        let now = Instant::now();
+        let elapsed_time = now.duration_since(start_time).as_secs_f64();
+
+        let dx = simulator.run();
+        println!("dx: {:?}", dx);
+
+        if elapsed_time >= 30.0 {
+            break;
+        }
     }
 }
