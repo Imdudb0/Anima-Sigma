d4280bd28cbf8a5928631954d29a066ea38559cb
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2026-01-02 13:25:03 -0500

    Update lib.rs

--- ArchT3/src/lib.rs
+++ ArchT3/src/lib.rs
@@ -5,3 +5,9 @@ pub use meta_cognition::reflex::{
     ReflexMetrics,
     ReflexConfig,
 };
+
+pub use cognition::neural_swarm::{
+    PrototypicalNeuralUnit,
+    TopologyConfig,
+    SignatureHandle,
+};
