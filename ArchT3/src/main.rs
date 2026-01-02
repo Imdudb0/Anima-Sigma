e92ff9ae70120130be4ad106b4adad950cf571f8
Imdudb0 <114720011+Imdudb0@users.noreply.github.com>
2026-01-02 09:25:05 -0500

    Update main.rs

--- ArchT3/src/main.rs
+++ ArchT3/src/main.rs
@@ -1,17 +1,9 @@
-use ArchT3::{
-BalanceStickAnimation,
-};
-
 fn main() {
-    let mut simulator = BalanceStickAnimation::new(30.0);
-    let start_time = Instant::now();
-   
     loop {
         let now = Instant::now();
         let elapsed_time = now.duration_since(start_time).as_secs_f64();
 
-        let dx = simulator.run();
-        println!("dx: {:?}", dx);
+        
 
         if elapsed_time >= 30.0 {
             break;
